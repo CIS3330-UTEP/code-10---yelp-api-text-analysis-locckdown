2d12119875efe5e690cb1daf631170f98f7c95b4
locckdown <143672719+locckdown@users.noreply.github.com>
2023-12-02 11:24:26 -0700

    Update code_10.py

--- code_10.py
+++ code_10.py
@@ -5,25 +5,29 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 api_key = 'Y0lVqprx4U-GuHAkhnDg_JKzfkDrDxDjHKno8cNOYnQHLSvS2dI7y-KtILRLIDaP20f57bQx0vW84eJll1RDsCAivXBbiB5Z1OuMkO7PFmkqHV9bNcfZOX-kud1MZXYx'
 yelp_api = YelpAPI(api_key)
 search_term = "sushi"
-cities = "El Paso, TX"
-location_term = cities
+city = "El Paso, TX"
+location_term = city
 analyzer = SentimentIntensityAnalyzer()
-df = pd.DataFrame(columns=["Alias", "Review"])
+df = pd.DataFrame(columns=["Name", "Alias", "Review"])
 
 search_results = yelp_api.search_query(
         term=search_term, location=location_term,
         sort_by='rating', limit=20)
 
 for item in search_results['businesses']:
+    business_name = item['name']
     review_response = yelp_api.reviews_query(id=item['alias'])
+    compound_sentiments = []
+    print(f"Restaurant: {business_name}")
     for review in review_response['reviews']:
-        df = df._append({"Alias": item['alias'], "Review": review['text']}, ignore_index=True)
+        review_text = review['text']
+        print(f"Review: {review_text}")
+        sentiment = analyzer.polarity_scores(review_text)
+        compound_sentiments.append(sentiment['compound'])
+        df = df._append({"Name": business_name, "Alias": item['alias'], "Review": review['text']}, ignore_index=True)
+    
+    average_sentiment_for_each = sum(compound_sentiments) / len(compound_sentiments) if compound_sentiments else 0
+    print(f"Average Compound Sentiment: {average_sentiment_for_each}")
+    print("\n")
 
-for index, row in df.iterrows():
-    name = row['Name']
-    review = row['Review']
-    print(f"Restaurant: {name}")
-    print(f"Review: {review}")
-    sentiment = analyzer.polarity_scores(review)
-    print(f"Sentiment: {sentiment}")
 
